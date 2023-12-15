5f2a102b309d83be478d1da8771b704e10a1a583
Althaf <kutagullaalthaf12@gmail.com>
2023-12-15 18:32:06 +0530

    TeamMapper created

---
+++ src/main/java/com/tproject/mappers/TeamMapper.java
@@ -0,0 +1,14 @@
+package com.tproject.mappers;
+
+import com.tproject.dto.TeamDto;
+import com.tproject.entity.Team;
+import org.mapstruct.Mapper;
+
+import java.util.Collection;
+@Mapper
+public interface TeamMapper {
+    TeamDto teamToDto(Team team);
+
+    Team DtoToTeam(TeamDto teamDto);
+    Collection<TeamDto> teamToDtoCollection(Collection<Team> teamCollection);
+}
