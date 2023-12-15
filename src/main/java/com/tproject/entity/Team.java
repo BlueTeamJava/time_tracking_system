e6069f32acb6ece90b6dbd3d0c11658b5a8e8c36
Althaf <kutagullaalthaf12@gmail.com>
2023-12-15 18:34:44 +0530

    Team entity created

---
+++ src/main/java/com/tproject/entity/Team.java
@@ -0,0 +1,11 @@
+package com.tproject.entity;
+import lombok.*;
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Team {
+    private int id;
+    String teamName;
+}
